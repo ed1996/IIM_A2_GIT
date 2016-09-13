04135a009eabec562ceed5e5fd9998dec9f26abb
Adam Alet <aletadam@gmail.com>
2016-09-13 12:45:07 +0200

    Checkpoint

--- login.php
+++ login.php
@@ -1,17 +1,23 @@
 <?php session_start();
-/********************************
-	DATABASE & FUNCTIONS
+
+/******************************** 
+	 DATABASE & FUNCTIONS 
 ********************************/
 require('config/config.php');
 require('model/functions.fn.php');
+
+
 /********************************
 			PROCESS
 ********************************/
+
 if(isset($_POST['email']) && isset($_POST['password'])){
 	if(!empty($_POST['email']) && !empty($_POST['password'])){
+
 		// TODO
+
 		// Force user connection to access dashboard
-		if (userConnection($db, $_POST['email'], $_POST['password']) == True) {
+		if (userConnection($db, $_POST['email'], $_POST['password']) == True){
 		
 		header('Location: dashboard.php');
 }
@@ -19,8 +25,9 @@ if(isset($_POST['email']) && isset($_POST['password'])){
 		$error = 'Champs requis !';
 	}
 }
-/********************************
-			VIEW
+
+/******************************** 
+			VIEW 
 ********************************/
 include 'view/_header.php';
 include 'view/login.php';
--- validation.php
+++ validation.php
@@ -6,7 +6,8 @@ session_start();
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
+	userRegistration($db, $_POST['username'], $_POST['email'], $_POST['password']); 
+	header('Location: login.php');
 
 }else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
