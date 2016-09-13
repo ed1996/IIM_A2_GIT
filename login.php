cfd38986a7511aac2d5fa6c13409b8bf8dc5074f
Adam Alet <aletadam@gmail.com>
2016-09-13 11:27:05 +0200

    MODIF LOGIN

--- login.php
+++ login.php
@@ -1,33 +1,26 @@
 <?php session_start();
-
-/******************************** 
-	 DATABASE & FUNCTIONS 
+/********************************
+	DATABASE & FUNCTIONS
 ********************************/
 require('config/config.php');
 require('model/functions.fn.php');
-
-
 /********************************
 			PROCESS
 ********************************/
-
 if(isset($_POST['email']) && isset($_POST['password'])){
 	if(!empty($_POST['email']) && !empty($_POST['password'])){
-
 		// TODO
-
 		// Force user connection to access dashboard
-		userConnection($db, 'git@initiation.com', 'password');
+		if (userConnection($db, $_POST['email'], $_POST['password']) == True) {
 		
 		header('Location: dashboard.php');
-
+}
 	}else{
 		$error = 'Champs requis !';
 	}
 }
-
-/******************************** 
-			VIEW 
+/********************************
+			VIEW
 ********************************/
 include 'view/_header.php';
 include 'view/login.php';
