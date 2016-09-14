6cc123ba1d8e37b23948dc98f2c314bc340376e3
Adam Alet <aletadam@gmail.com>
2016-09-14 10:24:27 +0200

    Bug

--- login.php
+++ login.php
@@ -20,16 +20,15 @@ if(isset($_POST['email']) && isset($_POST['password'])){
 		if (userConnection($db, $_POST['email'], $_POST['password']) == True){
 		
 		header('Location: dashboard.php');
-<<<<<<< HEAD
+
 	}
 
-=======
 }
->>>>>>> cfd38986a7511aac2d5fa6c13409b8bf8dc5074f
+
 	}else{
 		$error = 'Champs requis !';
 	}
-}
+
 
 /******************************** 
 			VIEW 
